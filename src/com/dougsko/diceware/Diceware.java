49e7817fa88087107d0e0e5352f07a88d980a348
dougsko <dougtko@gmail.com>
2011-06-09 20:05:13 -0400

    working on getting db stuff right

--- src/com/dougsko/diceware/Diceware.java
+++ src/com/dougsko/diceware/Diceware.java
@@ -1,13 +1,24 @@
 package com.dougsko.diceware;
 
+
 import android.app.Activity;
 import android.os.Bundle;
 
 public class Diceware extends Activity {
     /** Called when the activity is first created. */
+
+	private DicewareDbAdapter mDbHelper;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        mDbHelper = new DicewareDbAdapter(this);
+        mDbHelper.open();
+        
+        
+        
+        
     }
 }
--- src/com/dougsko/diceware/DicewareDbAdapter.java
+++ src/com/dougsko/diceware/DicewareDbAdapter.java
@@ -1,10 +1,16 @@
 package com.dougsko.diceware;
 
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import android.content.Context;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
@@ -17,7 +23,7 @@ public class DicewareDbAdapter {
 
     private static final String TAG = "DicewareDbAdapter";
     private DatabaseHelper mDbHelper;
-    private SQLiteDatabase mDb;
+    private static SQLiteDatabase mDb;
 
     /**
      * Database creation sql statement
@@ -27,7 +33,7 @@ public class DicewareDbAdapter {
     	+ "number VARCHAR(50), word VARCHAR(50));";
         
 
-    private static final String DATABASE_PATH = "/data/data/com.dougsko.diceware/";
+    private static final String DATABASE_PATH = "/data/data/com.dougsko.diceware/databases/";
     private static final String DATABASE_NAME = "diceware.db";
     private static final String DATABASE_TABLE = "words";
     private static final int DATABASE_VERSION = 2;
@@ -36,13 +42,16 @@ public class DicewareDbAdapter {
 
     private static class DatabaseHelper extends SQLiteOpenHelper {
 
-        DatabaseHelper(Context context) {
+        private Context mCtx;
+
+
+		DatabaseHelper(Context context) {
             super(context, DATABASE_NAME, null, DATABASE_VERSION);
         }
 
         @Override
         public void onCreate(SQLiteDatabase db) {
-            db.execSQL(DATABASE_CREATE);
+            //db.execSQL(DATABASE_CREATE);
         }
         
         @Override
@@ -51,6 +60,68 @@ public class DicewareDbAdapter {
                     + newVersion + ", which will destroy all old data");
             db.execSQL("DROP TABLE IF EXISTS words");
             onCreate(db);
+        }
+        
+        // this is crap i copied.
+        public void createDataBase() throws IOException{
+        	boolean dbExist = checkDataBase();
+        	if(dbExist){
+        		// do nothing, db exists
+        	}
+        	else{
+        		//By calling this method and empty database will be created into the default system path
+                //of your application so we are gonna be able to overwrite that database with our database.
+        		this.getReadableDatabase();
+        		try {
+        			copyDataBase();
+        		} 
+        		catch (IOException e) {
+        			throw new Error("Error copying database");
+        		}
+        	}
+        }
+        
+        private boolean checkDataBase(){
+        	SQLiteDatabase checkDB = null;
+        	try{
+        		String myPath = DATABASE_PATH + DATABASE_NAME;
+        		checkDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+        	}catch(SQLiteException e){
+        		//database does't exist yet.
+        	}
+        	if(checkDB != null){
+        		checkDB.close();
+        	}
+        	return checkDB != null ? true : false;
+        }
+    
+        private void copyDataBase() throws IOException{
+        	//Open your local database as the input stream
+        	InputStream myInput = mCtx.getAssets().open(DATABASE_NAME);
+        	// Path to the just created empty database
+        	String outFileName = DATABASE_PATH + DATABASE_NAME;
+        	
+        	//Open the empty db as the output stream
+        	OutputStream myOutput = new FileOutputStream(outFileName);
+        	
+        	//transfer bytes from the input file to the output file
+        	byte[] buffer = new byte[1024];
+        	int length;
+        	while ((length = myInput.read(buffer))>0){
+        		myOutput.write(buffer, 0, length);
+        	}
+     
+        	//Close the streams
+        	myOutput.flush();
+        	myOutput.close();
+        	myInput.close();
+        }
+        
+        public void openDataBase() throws SQLException{
+        	//Open the database
+            String myPath = DATABASE_PATH + DATABASE_NAME;
+        	mDb = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+        	
         }
     }
     
@@ -75,13 +146,27 @@ public class DicewareDbAdapter {
      */
     public DicewareDbAdapter open() throws SQLException {
         mDbHelper = new DatabaseHelper(mCtx);
-        mDb = mDbHelper.getWritableDatabase();
+        //mDb = mDbHelper.getReadableDatabase();
+        //return this;
+        
+        // this is mine
+        try {
+        	mDbHelper.createDataBase();
+        } catch (IOException ioe) {
+        	throw new Error("Unable to create database");
+        }
+        try {
+        	mDbHelper.openDataBase();
+        }catch(SQLException sqle){
+        	throw sqle;
+        }
         return this;
     }
     
     public void close() {
         mDbHelper.close();
     }
+    
     
     /**
      * Return a Cursor positioned at the note that matches the given rowId
