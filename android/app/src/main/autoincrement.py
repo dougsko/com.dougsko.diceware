944ab2de8538be23c354f65044957761d354b4e5
Doug Prostko <dougtko@gmail.com>
2020-07-24 17:34:30 -0400

    working on autoincrementing version

--- android/app/src/main/autoincrement.py
+++ android/app/src/main/autoincrement.py
@@ -9,6 +9,6 @@ versionNumbers = oldVersion.split('.')
 versionNumbers[-1] = str(int(versionNumbers[-1]) + 1)
 dom1.documentElement.setAttribute("android:versionName", u'.'.join(versionNumbers))
 dom1.documentElement.setAttribute("android:versionCode", str(int(oldVersionCode)+1))
-with open("AndroidManifest.xml", 'wb') as f:
+with open("AndroidManifest.xml", 'w') as f:
     for line in dom1.toxml("utf-8"):
         f.write(line)
