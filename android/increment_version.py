c3f099871c93a214ea92ca3ba12472d84ce80b6c
Doug Prostko <dougtko@gmail.com>
2020-07-24 19:35:24 -0400

    working on autoincrementing version

--- android/increment_version.py
+++ android/increment_version.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+from xml.dom.minidom import parse
 
 with open("local.properties.new", 'w') as newf:
     with open("local.properties") as f:
@@ -16,6 +17,18 @@ with open("local.properties.new", 'w') as newf:
             newf.write('%s=%s\n' % (key, value))
 
 os.rename('local.properties.new', 'local.properties')
+
+dom1 = parse("app/src/main/AndroidManifest.xml")
+oldVersion = dom1.documentElement.getAttribute("android:versionName")
+oldVersionCode = dom1.documentElement.getAttribute("android:versionCode")
+versionNumbers = oldVersion.split('.')
+
+versionNumbers[-1] = str(int(versionNumbers[-1]) + 1)
+dom1.documentElement.setAttribute("android:versionName", u'.'.join(versionNumbers))
+dom1.documentElement.setAttribute("android:versionCode", str(int(oldVersionCode)+1))
+with open("app/src/main/AndroidManifest.xml", 'w') as f:
+    for line in dom1.toxml("utf-8"):
+        f.write(str(line))
 
 
 
