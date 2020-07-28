6ff11b15cb0176598e37f6156d25c0d8af6f3203
Doug Prostko <dougtko@gmail.com>
2020-07-28 16:32:34 -0400

    update main.yml

---
+++ increment_version.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# uses the version name and code from pubspec.yaml and updates
+# AndroidManifest.xml
+
+from xml.dom.minidom import parse
+
+with open("pubspec.yaml") as f:
+    for line in f.readlines():
+        option = line.split(":")
+        print(option)
+        if option[0] == "version":
+            version = option[1].split("+")
+            versionName = version[0].lstrip()
+            versionCode = version[1].rstrip()
+
+dom1 = parse("android/app/src/main/AndroidManifest.xml")
+dom1.documentElement.setAttribute("android:versionName", versionName)
+dom1.documentElement.setAttribute("android:versionCode", versionCode)
+with open("android/app/src/main/AndroidManifest.xml", 'w') as f:
+    dom1.writexml(f)
+
+
+
